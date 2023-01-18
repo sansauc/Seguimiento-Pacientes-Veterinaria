7794f02c2930f8e266fdacd92f9494027a10d228
sansauc <sansauc@gmail.com>
2023-01-18 16:23:49 -0300

    Se implemento la modificacion del encabezado una vez de acuerdo a si la lista de pacientes esta vacia o cargada

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,38 +1,56 @@
 import Paciente from "./Paciente";
 
-const ListadoPacientes = ({pacientes}) => { //extraigo los pacientes que le paso con el props
- 
+const ListadoPacientes = ({ pacientes }) => { //extraigo los pacientes que le paso con el props
+
 
   return (
     <div className="md:w-1/2 lg:w-3/5" > {/* el md se puede ver cuando se va achicando la pantalla, tambien aca puedo agregar h-screen: fijamos un tamaño y con overflow-y-scroll: agregamos un scroll vertical*/}
-      <h2 className="font-black text-xl text-center">
-        Listado de Pacientes
-      </h2>
-      <p className="mt-5 text-lg text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold">
-          Pacientes y Citas
-        </span>
-      </p>
 
-     
-      <div className="md:h-screen overflow-y-scroll"> {/*Otra opcion es crean un div para los componentes y aca meter el h-screen: fijamos un tamaño y con overflow-y-scroll: agregamos un scroll vertical */}
-        
+      {pacientes && pacientes.length ? (
 
-        {pacientes.map (  (paciente) => 
-            <Paciente 
-              //No se recomienda pasar el indice como key, para ello podemos generar uno combinando la fecha y la funcion math.random
-
-              key={paciente.id} //Siempre que se use un map, hay que pasar un key que seria el indice creado cuando se genera un objeto
-              paciente={paciente}
-
-            /> /* Importamos el compomente pasando como props al paciente, agrega uno por cada paciente se haya cargado */
-        ) }
-  
+        <>
+          <h2 className="font-black text-xl text-center">
+            Listado de Pacientes
+          </h2>
+          <p className="mt-5 text-lg text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">
+              Pacientes y Citas
+            </span>
+          </p>
 
 
+          <div className="md:h-screen overflow-y-scroll"> {/*Otra opcion es crean un div para los componentes y aca meter el h-screen: fijamos un tamaño y con overflow-y-scroll: agregamos un scroll vertical */}
 
-      </div>
+
+            {pacientes.map((paciente) =>
+              <Paciente
+                //No se recomienda pasar el indice como key, para ello podemos generar uno combinando la fecha y la funcion math.random
+
+                key={paciente.id} //Siempre que se use un map, hay que pasar un key que seria el indice creado cuando se genera un objeto
+                paciente={paciente}
+
+              /> /* Importamos el compomente pasando como props al paciente, agrega uno por cada paciente se haya cargado */
+            )}
+          </div>
+        </>)
+
+        : (
+          <>
+
+            <h2 className="font-black text-xl text-center">
+              No Hay Pacientes
+            </h2>
+            <p className="mt-5 text-lg text-center">
+              Agrega pacientes y {""}
+              <span className="text-indigo-600 font-bold">
+                se listarán a continuación:
+              </span>
+            </p>
+          </>
+        )}
+
+
 
     </div>
   )
